8783a07851b8a2fc89bdc5d425efcbc0339030a5
deptcalidad <dptocalidad@versuselearning.com>
2025-05-21 14:17:16 +0200

    up

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\carlo\Documents\GitHub\waxhub_v3"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Carlos\Documents\GitHub\waxhub_v3"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
